650bd92c4e74e8a4180e1c62658319ef6cbc94f8
jisstjp <thomas.jose.panicker@gmail.com>
2023-04-07 00:13:15 -0500

    domin changes

--- StockKeeper/src/main/java/com/stock/inventorymanagement/domain/User.java
+++ StockKeeper/src/main/java/com/stock/inventorymanagement/domain/User.java
@@ -1,12 +1,12 @@
 package com.stock.inventorymanagement.domain;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -15,25 +15,24 @@ import lombok.Setter;
 @Getter
 @Setter
 public class User {
-	
+
 	@Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 
-    @Column(nullable = false, unique = true)
-    private String username;
+	@Column(nullable = false, unique = true)
+	private String username;
 
-    @Column(nullable = false)
-    private String password;
+	@Column(nullable = false)
+	private String password;
 
-    @Column(nullable = false, unique = true)
-    private String email;
+	@Column(nullable = false, unique = true)
+	private String email;
 
-    @Column(name = "first_name")
-    private String firstName;
+	@Column(name = "first_name")
+	private String firstName;
 
-    @Column(name = "last_name")
-    private String lastName;
-
+	@Column(name = "last_name")
+	private String lastName;
 
 }
