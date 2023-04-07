ebca877f925a3c26b5bd1513c3c56c7ecb51e955
jisstjp <thomas.jose.panicker@gmail.com>
2023-04-07 01:57:10 -0500

    repo-user

--- StockKeeper/src/main/java/com/stock/inventorymanagement/StockKeeperApplication.java
+++ StockKeeper/src/main/java/com/stock/inventorymanagement/StockKeeperApplication.java
@@ -2,8 +2,10 @@ package com.stock.inventorymanagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.stock.inventorymanagemen.domain")
 public class StockKeeperApplication {
 
 	public static void main(String[] args) {
