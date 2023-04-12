f888adb94cce551e58e58ae27d081561aebae888
jisstjp <thomas.jose.panicker@gmail.com>
2023-04-12 01:18:33 -0500

    jwttoken

---
+++ StockKeeper/src/main/java/com/stock/inventorymanagement/util/JwtTokenUtil.java
@@ -0,0 +1,64 @@
+package com.stock.inventorymanagement.util;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+@Component
+public class JwtTokenUtil {
+	
+	
+	  @Value("${jwt.secret}")
+	    private String secret;
+
+	    @Value("${jwt.expiration}")
+	    private long expiration;
+
+	    public String generateToken(UserDetails userDetails) {
+	        Map<String, Object> claims = new HashMap<>();
+	        return Jwts.builder()
+	                .setClaims(claims)
+	                .setSubject(userDetails.getUsername())
+	                .setIssuedAt(new Date())
+	                .setExpiration(new Date(System.currentTimeMillis() + expiration * 1000))
+	                .signWith(SignatureAlgorithm.HS256, secret)
+	                .compact();
+	    }
+
+	    public String getUsernameFromToken(String token) {
+	        return getClaimFromToken(token, Claims::getSubject);
+	    }
+
+	    public Date getExpirationDateFromToken(String token) {
+	        return getClaimFromToken(token, Claims::getExpiration);
+	    }
+
+	    public <T> T getClaimFromToken(String token, Function<Claims, T> claimsResolver) {
+	        Claims claims = getAllClaimsFromToken(token);
+	        return claimsResolver.apply(claims);
+	    }
+
+	    private Claims getAllClaimsFromToken(String token) {
+	        return Jwts.parser().setSigningKey(secret).parseClaimsJws(token).getBody();
+	    }
+
+	    public boolean validateToken(String token, UserDetails userDetails) {
+	        String username = getUsernameFromToken(token);
+	        return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
+	    }
+
+	    private boolean isTokenExpired(String token) {
+	        Date expirationDate = getExpirationDateFromToken(token);
+	        return expirationDate.before(new Date());
+	    }
+
+}
