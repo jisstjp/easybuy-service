eb6ea791cc80f191b629afdbf188159fe5ce2ef2
jisstjp <thomas.jose.panicker@gmail.com>
2023-05-15 13:58:15 -0500

    cors support

--- StockKeeper/src/main/java/com/stock/inventorymanagement/config/SecurityConfigurer.java
+++ StockKeeper/src/main/java/com/stock/inventorymanagement/config/SecurityConfigurer.java
@@ -3,6 +3,8 @@ package com.stock.inventorymanagement.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -12,14 +14,13 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-
 import com.stock.inventorymanagement.security.JwtAuthenticationEntryPoint;
 import com.stock.inventorymanagement.security.JwtRequestFilter;
 import com.stock.inventorymanagement.service.impl.UserDetailsServiceImpl;
 
 @Configuration
 @EnableWebSecurity
+@Import(CorsConfig.class)
 public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
 
 
@@ -51,10 +52,12 @@ public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
         httpSecurity.csrf().disable().
-               authorizeRequests().antMatchers("/api/v1/auth/login").permitAll().
+               authorizeRequests().antMatchers(HttpMethod.OPTIONS,"/api/v1/auth/login").permitAll().
                         anyRequest().authenticated().and().
                         exceptionHandling().authenticationEntryPoint(jwtAuthenticationEntryPoint).and().sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        
+        httpSecurity.cors();
 
         httpSecurity.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
     }
